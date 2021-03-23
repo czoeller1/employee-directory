697eee8938de0ee88d489c0f725a2a1005b39fc6
Charles Zoeller <charles.zoeller1@gmail.com>
2021-03-22 18:59:59 -0600

    added table

--- src/pages/Table.js
+++ src/pages/Table.js
@@ -1,10 +1,114 @@
 import React, { useEffect, useState } from "react";
 import API from "../util/API";
+import Row from "../components/Row";
 
 function Table() {
-  API.fetchUsers();
+  const [employees, setEmployees] = useState();
+  const [display, changeDisplay] = useState();
 
-  return <div>Test Axios</div>;
+  useEffect(() => {
+    loadEmployees();
+  }, []);
+
+  function loadEmployees() {
+    API.fetchUsers()
+      .then((users) => {
+        setEmployees(users);
+        changeDisplay(users);
+      })
+
+      .catch((err) => console.log(err));
+  }
+
+  const filterDisplay = (e) => {
+    changeDisplay(
+      employees.filter((employee) => employee.gender === e.target.id)
+    );
+  };
+
+  const sortDisplay = (e) => {
+    let ages = employees.map((a) => a);
+    ages.sort((a, b) => (e.target.id === "<" ? a.age - b.age : b.age - a.age));
+    changeDisplay(ages);
+  };
+
+  const clearFilter = () => {
+    changeDisplay(employees);
+  };
+
+  //console.log(employees);
+  return (
+    <div className="w-50 mx-auto">
+      <div className=" d-flex justify-content-between">
+        <div className="dropdown">
+          <button
+            className="btn btn-secondary dropdown-toggle"
+            type="button"
+            id="filter"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            Filter by Gender
+          </button>
+          <div className="dropdown-menu" aria-labelledby="filter">
+            <button className="dropdown-item" id="male" onClick={filterDisplay}>
+              Male
+            </button>
+            <hr></hr>
+            <button
+              className="dropdown-item"
+              id="female"
+              onClick={filterDisplay}
+            >
+              Female
+            </button>
+          </div>
+        </div>
+        <button className="btn btn-secondary" onClick={clearFilter}>
+          Reset
+        </button>
+        <div className="dropdown">
+          <button
+            className="btn btn-secondary dropdown-toggle"
+            type="button"
+            id="sort"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            Sort by Age
+          </button>
+          <div className="dropdown-menu" aria-labelledby="sort">
+            <button className="dropdown-item" id="<" onClick={sortDisplay}>
+              Ascending
+            </button>
+            <hr></hr>
+            <button className="dropdown-item" id=">" onClick={sortDisplay}>
+              Descending
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <table className="table mt-5">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Gender</th>
+            <th scope="col">Age</th>
+            <th scope="col">Email</th>
+            <th scope="col">Picture</th>
+          </tr>
+        </thead>
+        <tbody>
+          <Row employees={display}></Row>
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default Table;
--- src/util/API.js
+++ src/util/API.js
@@ -1,14 +1,15 @@
 import axios from "axios";
 
 // Export an object containing methods we'll use for accessing the random user API
-export default {
+const ret = {
   fetchUsers: function () {
     return axios
       .get(
-        "https://randomuser.me/api/?results=20&inc=gender,name,dob,picture,email"
+        "https://randomuser.me/api/?results=20&inc=gender,name,dob,picture,email,id&nat=US,GB"
       )
       .then((res) => {
         let users = res.data.results; // name.first, name.last, gender, email, dob.age, picture.thumbnail
+        let id = 1;
         users = users.map((user) => {
           return {
             first: user.name.first,
@@ -17,10 +18,12 @@ export default {
             email: user.email,
             age: user.dob.age,
             picture: user.picture.thumbnail,
+            id: id++,
           };
         });
-        console.log(users);
+        //console.log(users);
         return users;
       });
   },
 };
+export default ret;
